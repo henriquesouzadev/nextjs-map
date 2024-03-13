13d04a0844ce1bbe93d029ce6c4a61490d59de84
henriquesouzadev <henriquesouzadev@gmail.com>
2024-03-13 20:47:23 -0300

    nezt

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import * as S from './styles'
 
 const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="/img/logo.svg" alt="Logo" />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration src="./img/hero-illustration.svg" alt="Illustration" />
+    </S.Wrapper>
   )
 }
 
